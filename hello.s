ca47a2e980b67da7228fb8c2ed3103bc86e448e2
Nicolas BAUW <nbauw@hotmail.com>
2024-02-07 15:52:33 +0100

    Return value + comments

--- hello.s
+++ hello.s
@@ -1,14 +1,14 @@
 .text
 .global _start
 _start:
-    li  a0, 1
-    la  a1, message
-    la  a2, len
-    li  a7, 64
+    li  a0, 1           # stdout
+    la  a1, message     # "hello world" string
+    la  a2, len         # string lenght
+    li  a7, 64          # "write" syscall
     ecall
 
-    li  a0, 2
-    li  a7, 93
+    mv  a0, x0          # return code 0
+    li  a7, 93          # "exit" syscall
     ecall
 
 .data
